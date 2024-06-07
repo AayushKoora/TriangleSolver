5788bb088377fc4784a3733844af66e68420346e
aayush koora <kooraaayush@gmail.com>
2024-06-06 23:11:27 -0600

    saved changes

---
+++ main.py
@@ -0,0 +1,22 @@
+import math
+from add_information import AddInformation
+from solve import Solve
+from show_results import ShowResults
+
+
+def main():
+
+
+    print('Welcome to the Triangle Solver, Input Provided Sides and Angles')
+    print('Place 0s in for Sides and Angles Not Known')
+   
+    AddInformation()
+
+
+
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
